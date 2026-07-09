6b24b705a7933a0472ab0133941bc292cf59cec6
Ayushi Choyal <524410017@nitkkr.ac.in>
2026-07-09 22:12:11 +0530

    Updated about section responsive design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import "./Navbar.css";
 const Navbar = () => {
   const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -13,6 +15,12 @@ const Navbar = () => {
         setShow(false);
       } else {
         setShow(true);
+      }
+
+      if (currentScrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
       }
 
       setLastScrollY(currentScrollY);
@@ -26,44 +34,61 @@ const Navbar = () => {
   }, [lastScrollY]);
 
   return (
-    <nav className={`navbar ${show ? "show" : "hide"}`}>
+    <nav
+      className={`navbar ${show ? "show" : "hide"} ${
+        scrolled ? "scrolled" : ""
+      }`}
+    >
+      <div className="logo">Ayushi Choyal</div>
 
-      <div className="logo">
-        Ayushi Choyal
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
       </div>
 
-      <ul className="nav-links">
-
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <a href="#home">HOME</a>
+          <a href="#home" onClick={() => setMenuOpen(false)}>
+            HOME
+          </a>
         </li>
 
         <li>
-          <a href="#about">ABOUT</a>
+          <a href="#about" onClick={() => setMenuOpen(false)}>
+            ABOUT
+          </a>
         </li>
 
         <li>
-          <a href="#skills">SKILLS</a>
+          <a href="#skills" onClick={() => setMenuOpen(false)}>
+            SKILLS
+          </a>
         </li>
 
         <li>
-          <a href="#projects">PROJECTS</a>
+          <a href="#projects" onClick={() => setMenuOpen(false)}>
+            PROJECTS
+          </a>
         </li>
 
         <li>
-          <a href="#education">EDUCATION</a>
+          <a href="#education" onClick={() => setMenuOpen(false)}>
+            EDUCATION
+          </a>
         </li>
 
         <li>
-          <a href="#journey">ACHIEVEMENTS</a>
+          <a href="#journey" onClick={() => setMenuOpen(false)}>
+            ACHIEVEMENTS
+          </a>
         </li>
-
       </ul>
 
       <a href="#contact" className="hire-btn">
         HIRE ME
       </a>
-
     </nav>
   );
 };
